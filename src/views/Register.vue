a9511fb5980226135961a50037aecf142a5d34ab
Danilo Novaes <57879816+danilo-1@users.noreply.github.com>
2021-10-27 21:57:29 -0300

    implementação de novos arquivos ao projeto

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,6 +3,7 @@
   <div>
     <h4 class="mt-2">Cadastro</h4>
     <div class="row">
+      <form @submit.prevent="register()">
       <div class="col-12 mb-2">
         <input type="text" v-model="user.name" placeholder="Nome" />
       </div>
@@ -13,34 +14,64 @@
         <input type="password" v-model="user.password" placeholder="Senha" />
       </div>
       <div class="col-12">
-        <button @click="register()">Cadastrar</button>
+        <button type="submit">Cadastrar</button>
       </div>
+      </form>
     </div>
+  </div>
+  <div v-if="verific == true">
+      <div class="alert alert-primary" role="alert">
+        Usuário cadastrado com sucesso!
+      </div>
+  </div>
+  <div v-if="error == true">
+      <div class="alert alert-danger" role="alert">
+        Erro ao se cadastrar!
+      </div>
   </div>
 </template>
 
 <script>
+import { toRefs, ref, reactive } from 'vue';
 import Header from "../components/Header.vue";
-
+import UserService from "../services/UserService"
 export default {
   name: "Register",
   components: {
+    UserService,
     Header,
   },
-  data() {
-    return {
+  setup(){
+    const verific = ref(false)
+    const error = ref(false)
+    const data = reactive({
       user: {
-        name: null,
-        email: null,
-        password: null,
+        name: "",
+        email: "",
+        password: "",
       },
-    };
-  },
-  methods: {
-    register() {
-      console.log(this.user);
-    },
-  },
+      
+      })
+      const methods = reactive({
+          register() {
+        UserService.register()
+        .then(()=>{
+          verific.value = true
+        })
+        .catch(()=>{
+          error.value = true
+        })
+        }
+      })
+    return{
+      verific,
+      error,
+      ...toRefs(data),
+      ...toRefs(methods)
+    }
+  }
+  
+  
 };
 </script>
 
