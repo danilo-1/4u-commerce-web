3e4ae1e0f370197f89a7a70d78f7132ed0714eb0
Danilo Novaes <57879816+danilo-1@users.noreply.github.com>
2021-11-29 22:19:55 -0300

    .gitignore liberado

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 var URL = "";
 
 /* URL = "http://localhost:8080"; */
-URL = "http://api-4ucommerce.herokuapp.com";
+URL = "https://api-4ucommerce.herokuapp.com/index.html";
 
 export default axios.create({
     baseURL: URL,
--- src/layouts/AppLayoutDashboard.vue
+++ src/layouts/AppLayoutDashboard.vue
@@ -1,10 +1,5 @@
 <template>
 <head>
-    <meta charset="UTF-8">
-    
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
 <body>
   <div class="sidebar" >
     <div class="logo-details">
