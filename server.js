bb17dc7477d85dfef1578a3d2bdcea012e384883
Danilo Novaes <57879816+danilo-1@users.noreply.github.com>
2021-11-29 22:56:23 -0300

    A

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
